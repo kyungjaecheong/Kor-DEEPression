44d8c5c60179b9c59d67d8b873dbd0d1555563c0
KyungJae Cheong <109939415+kyungjaecheong@users.noreply.github.com>
2023-01-29 13:27:26 +0900

    update database 230129 (#11)
    Closes #10

--- data/custom_modules/preprocess.py
+++ data/custom_modules/preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-col_demo = ['year', 'age', 'HE_ht', 'HE_wt', 'HE_BMI', 'sex', 'edu', 'genertn', 'marri_2', 'EC1_1']
+col_demo = ['year', 'age', 'HE_BMI', 'sex', 'edu', 'genertn', 'marri_2', 'EC1_1']
 col_health = ['D_1_1', 'LQ4_00', 'D_2_1', 'BO1_1', 'BO2_1']
 col_life = ['BD1_11', 'BD2_1', 'sm_presnt', 'BP1']
 col_disease = ['HE_HP', 'HE_DM', 'HE_HCHOL', 'HE_HTG']
@@ -20,15 +20,6 @@ def data_load_20(filedir):
     df1 = df0[col_list].rename(columns={'ID':'id','HE_DM_HbA1c':'HE_DM'})
     return df1
 
-# df14 = data_load('./data/downloads/HN14_ALL.csv')
-# print(df14.shape)
-# df16 = data_load('./data/downloads/HN16_ALL.csv')
-# print(df16.shape)
-# df18 = data_load('./data/downloads/HN18_ALL.csv')
-# print(df18.shape)
-# df20 = data_load('./data/downloads/HN20_ALL.csv')
-# print(df20.shape)
-
 def fill_nan(value):
     if value == ' ':
         value = None
@@ -45,35 +36,13 @@ def drop_nan_df(df):
     df1 = df1.dropna(axis=0, how='any')
     return df1
 
-# df14 = data_load('./data/downloads/HN14_ALL.csv')
-# df14_drop = drop_nan_df(df14)
-# print(df14_drop.shape)
-# df16 = data_load('./data/downloads/HN16_ALL.csv')
-# df16_drop = drop_nan_df(df16)
-# print(df16_drop.shape)
-# df18 = data_load('./data/downloads/HN18_ALL.csv')
-# df18_drop = drop_nan_df(df18)
-# print(df18_drop.shape)
-# df20 = data_load('./data/downloads/HN20_ALL.csv')
-# df20_drop = drop_nan_df(df20)
-# print(df20_drop.shape)
-
 def concat_df(df_list):
     df = pd.concat(df_list, ignore_index=True)
     return df
 
-# df14 = data_load('./data/downloads/HN14_ALL.csv')
-# df16 = data_load('./data/downloads/HN16_ALL.csv')
-# df18 = data_load('./data/downloads/HN18_ALL.csv')
-# df20 = data_load('./data/downloads/HN20_ALL.csv')
-# df_list = [df14, df16, df18, df20]
-# df = concat_df(df_list)
-# df_drop = drop_nan_df(df)
-# print(df_drop.shape)
-
 def query_df(df):
-    df1 = df.query('HE_BMI<40 and marri_2!=99 and marri_2!=8 and marri_2!=9')
-    df2 = df1.query('D_1_1!=9 and BO1_1!=9 and BD1_11!=9 and BD2_1!=9 and BP1!=9')
+    df1 = df.query('marri_2!=99 and marri_2!=8 and marri_2!=9 and D_1_1!=9')
+    df2 = df1.query('BO1_1!=9 and BD1_11!=9 and BD2_1!=9 and BP1!=9')
     return df2
 
 def get_targets(df):
@@ -98,13 +67,12 @@ def get_targets(df):
 def get_features(df):
     household = []
     marital = []
-    # health = []
     limit = []
     modality = []
     w_change = []
-    # w_control = []
     HE_HBP = []
     HE_DB = []
+    HE_DYSL = []
     drink_freq = []
     drink_amount = []
     
@@ -124,14 +92,7 @@ def get_features(df):
             marital.append(2)
         elif df.loc[i, 'marri_2'] == 88:
             marital.append(3)
-        
-        # if df.loc[i, 'D_1_1'] in [1,2]:
-        #     health.append(1)
-        # elif df.loc[i, 'D_1_1'] == 3:
-        #     health.append(2)
-        # elif df.loc[i, 'D_1_1'] in [4,5]:
-        #     health.append(3)
-        
+                
         if df.loc[i, 'LQ4_00'] == 2:
             limit.append(0)
         elif df.loc[i, 'LQ4_00'] == 1:
@@ -150,12 +111,7 @@ def get_features(df):
         elif df.loc[i, 'BO1_1'] in [2,3]\
         and df.loc[i, 'BO2_1'] == 4:
             w_change.append(1)
-        
-        # if df.loc[i, 'BO2_1'] in [1,2,3]:
-        #     w_control.append(1)
-        # elif df.loc[i, 'BO2_1'] == 4:
-        #     w_control.append(0)
-        
+                
         if df.loc[i, 'HE_HP'] == 3:
             HE_HBP.append(1)
         elif df.loc[i, 'HE_HP'] in [1,2]:
@@ -166,25 +122,31 @@ def get_features(df):
         elif df.loc[i, 'HE_DM'] in [1,2]:
             HE_DB.append(0)
         
-        if df.loc[i, 'BD1_11'] in [1,2,8]:
+        if df.loc[i, 'HE_HCHOL'] == 1\
+        or df.loc[i, 'HE_HTG'] == 1:
+            HE_DYSL.append(1)
+        elif df.loc[i, 'HE_HCHOL'] == 0\
+        and df.loc[i, 'HE_HTG'] == 0:
+            HE_DYSL.append(0)
+        
+        if df.loc[i, 'BD1_11'] == 8:
             drink_freq.append(0)
-        elif df.loc[i, 'BD1_11'] in [3,4,5,6]:
-            drink_freq.append(df.loc[i, 'BD1_11']-2)
+        elif df.loc[i, 'BD1_11'] in [1,2,3,4,5,6]:
+            drink_freq.append(df.loc[i, 'BD1_11'])
         
         if df.loc[i, 'BD2_1'] == 8:
             drink_amount.append(0)
-        elif df.loc[i, 'BD2_1'] !=8:
+        elif df.loc[i, 'BD2_1'] in [1,2,3,4,5]:
             drink_amount.append(df.loc[i, 'BD2_1'])
     
     df['household'] = household
     df['marital'] = marital
-    # df['health'] = health
     df['limit'] = limit
     df['modality'] = modality
     df['w_change'] = w_change
-    # df['w_control'] = w_control
     df['HE_HBP'] = HE_HBP
     df['HE_DB'] = HE_DB
+    df['HE_DYSL'] = HE_DYSL
     df['dr_freq'] = drink_freq
     df['dr_amount'] = drink_amount
     
--- data/data-prepare.py
+++ data/data-prepare.py
@@ -6,23 +6,25 @@ df14 = preprocess.data_load('./data/downloads/HN14_ALL.csv')
 df16 = preprocess.data_load('./data/downloads/HN16_ALL.csv')
 df18 = preprocess.data_load('./data/downloads/HN18_ALL.csv')
 df20 = preprocess.data_load_20('./data/downloads/HN20_ALL.csv')
+print('Data loaded successfully')
 
 df_list = [df14, df16, df18, df20]
 df = preprocess.concat_df(df_list)
 df_drop = preprocess.drop_nan_df(df)
-df_drop.to_csv('./data/downloads/ver3/HN_drop_14_20.csv', index=False)
+df_drop.to_csv('./data/downloads/ver4/HN_drop_14_20.csv', index=False)
+print('Droped nan values successfully')
 
-df_test = pd.read_csv('./data/downloads/ver3/HN_drop_14_20.csv')
+df_test = pd.read_csv('./data/downloads/ver4/HN_drop_14_20.csv')
 
 df_query = preprocess.query_df(df_test).reset_index(drop=True)
 df_add_targets = preprocess.get_targets(df_query)
 df_add_features = preprocess.get_features(df_add_targets)
+print('Query & Add Features & Add Targets successfully')
 
 col_year = ['id', 'year']
-col_feature = ['id', 'age', 'HE_ht', 'HE_wt', 'HE_BMI',
-               'sex', 'edu', 'household', 'marital', 'EC1_1',
+col_feature = ['id', 'age', 'HE_BMI', 'sex', 'edu', 'household', 'marital', 'EC1_1',
                'D_1_1', 'limit', 'modality', 'w_change',
-               'HE_HBP', 'HE_DB', 'HE_HCHOL', 'HE_HTG',
+               'HE_HBP', 'HE_DB', 'HE_DYSL',
                'dr_freq', 'dr_amount', 'sm_presnt', 'BP1']
 col_target = ['id', 'Depression', 'MDD']
 
@@ -30,9 +32,13 @@ df_year = df_add_features[col_year]
 df_feature = df_add_features[col_feature]
 df_target = df_add_features[col_target]
 
-df_year.to_csv('./data/downloads/ver3/HN_year.csv', index=False)
-df_feature.to_csv('./data/downloads/ver3/HN_feature.csv', index=False)
-df_target.to_csv('./data/downloads/ver3/HN_target.csv', index=False)
+df_year.to_csv('./data/downloads/ver4/HN_year.csv', index=False)
+df_feature.to_csv('./data/downloads/ver4/HN_feature.csv', index=False)
+df_target.to_csv('./data/downloads/ver4/HN_target.csv', index=False)
+
+print('Data division completed')
+print('Shapes : df_year, df_feature, df_target')
+print(df_year.shape, df_feature.shape, df_target.shape)
 
 # ---------TEST CODES---------
 # df14 = preprocess.data_load('./data/downloads/HN14_ALL.csv')
