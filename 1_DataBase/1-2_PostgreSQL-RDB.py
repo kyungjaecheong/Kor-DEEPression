18f4de2f4cd9c6f3ac187e9c427d7ac134a7e928
KyungJae Cheong <dankj1991@gmail.com>
2023-02-01 10:20:47 +0900

    주석 추가 완료 230201

--- 1_DataBase/1-2_PostgreSQL-RDB.py
+++ 1_DataBase/1-2_PostgreSQL-RDB.py
@@ -3,21 +3,27 @@ CLI 실행 명령어
 $ python 1_DataBase/1-2_PostgreSQL-RDB.py
 '''
 
+# 라이브러리 및 함수 Impoert
 import os
 import sys
 import psycopg2
 from dotenv import load_dotenv
 from custom_modules.postgresql_upload import *
 
+# .env파일에 저장된 정보들을 불러옴
 load_dotenv(verbose=True)
 
+# HOST, PORT, DATABASE, USERNAME, PASSWORD를 전역변수로 저장
 HOST = os.getenv('postgre_host')
 PORT = 5432
 DATABASE = os.getenv('postgre_database')
 USERNAME = os.getenv('postgre_user')
 PASSWORD = os.getenv('postgre_password')
 
+
+# 파일이 실행되면 자동으로 동작하는 main 함수
 def main():
+    
     # postgreSQL 연결
     try:
         conn = psycopg2.connect(
@@ -26,12 +32,17 @@ def main():
             database=DATABASE,
             user=USERNAME,
             password=PASSWORD)
+        # 커서 지정
         cur = conn.cursor()
-        print('connection success to DB')
+        print('\nconnection success to DB')
+    
+    # 예외처리 : 연결 실패할 경우 시스템을 중단
     except:
-        print('connection error to DB')
+        print('\nconnection error to DB')
         sys.exit()
 
+
+    # 테이블 리스트 정의 (id_year-->targets-->범주형변수들-->features)
     table_list = ['id_year',
                   'targets',
                   'sex',
@@ -46,25 +57,33 @@ def main():
                   'stress',
                   'features']
     
-    # 테이블 초기화
+    
+    # 테이블 초기화 (features-->범주형변수들-->targets-->id_year)
     table_initialization(cursor=cur, table_list=table_list)
-    print('table initialization complete')
+    print('\ntable initialization complete')
     
-    # 테이블 생성
+    
+    # 테이블 생성 (id_year-->targets-->범주형변수들-->features)
     table_creation(cursor=cur, table_list=table_list)
-    print('table created complete')
+    print('\ntable created complete')
     
-    # 데이터 삽입
+    
+    # 데이터 삽입 (id_year-->targets-->범주형변수들-->features)
     insertion_csv(cur, table_list[0], path='./1_DataBase/downloads/HN_year.csv')
     insertion_csv(cur, table_list[1], path='./1_DataBase/downloads/HN_target.csv')
     insertion_json(cur, table_list[2:-1], path='./1_DataBase/json_variable.json')
     insertion_csv(cur, table_list[-1], path='./1_DataBase/downloads/HN_feature.csv')
-    print('data insert complete')
+    print('\ndata insert complete')
 
+
+    # 변경사항을 저장(commit)
     conn.commit()
-    conn.close()
     
-    print('Successfully Disconnected to DB')
-    
+    # postgreSQL 연결 종료
+    conn.close()    
+    print('\nSuccessfully Disconnected to DB\n')
+
+
+# 파일이 실행되면 자동으로 main 함수를 동작하도록 함  
 if __name__ == '__main__':
     main()
--- 1_DataBase/custom_modules/postgresql_upload.py
+++ 1_DataBase/custom_modules/postgresql_upload.py
@@ -8,9 +8,19 @@ Project_repo_url : https://github.com/kyungjaecheong/Kor-DEEPression
 Contributor : Kyung Jae, Cheong (정경재)
 '''
 
+# 함수 리스트 및 __all__ 정의(import * 할 때 불러올 함수들을 정의)
+# from custom_modules.preprocess import *
+__all__ = ['table_initialization',
+           'table_creation',
+           'insertion_csv',
+           'insertion_json']
+
+# 라이브러리 import
 import csv
 import json
 
+# 테이블 초기화 기능(features-->범주형변수들-->targets-->id_year)
+# 외래키를 가진 테이블(자식)부터 제거해야하므로 순서가 중요함
 def table_initialization(cursor, table_list):
     '''
     table_initialization
@@ -22,10 +32,15 @@ def table_initialization(cursor, table_list):
     ---
     출력 : None 
     '''
+    
+    # 입력 받은 table list의 역순으로 반복을 진행함
     for table_name in reversed(table_list):
+        # 테이블 이름과 동일한 테이블이 존재하면 삭제하는 명령어 실행 
         cursor.execute(f"""DROP TABLE IF EXISTS {table_name};""")
 
 
+# 테이블 생성 기능(id_year-->targets-->범주형변수들-->features)
+# 외래키를 가지지 않는 테이블(부모)부터 생성해야하므로 순서가 중요함
 def table_creation(cursor, table_list):
     '''
     table_creation
@@ -37,12 +52,15 @@ def table_creation(cursor, table_list):
     ---
     출력 : None 
     '''
-
+    
+    # id_year 테이블 생성 (부모 테이블)
     cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_list[0]} (
         id VARCHAR NOT NULL,
         year INTEGER,
         CONSTRAINT {table_list[0]}_pk PRIMARY KEY (id)
         );""")
+    
+    # targets 테이블 생성 (id_year의 자식 테이블)
     cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_list[1]} (
         id VARCHAR NOT NULL,
         depression INTEGER,
@@ -50,12 +68,16 @@ def table_creation(cursor, table_list):
         CONSTRAINT {table_list[1]}_pk PRIMARY KEY (id),
         CONSTRAINT {table_list[1]}_fk FOREIGN KEY (id) REFERENCES {table_list[0]} (id)
         );""")
+    
+    # 범주형 변수들(총 10개)의 테이블들을 생성 (부모 테이블)
     for feature_name in table_list[2:-1]:
         cursor.execute(f"""CREATE TABLE IF NOT EXISTS {feature_name} (
             id_{feature_name} INTEGER,
             {feature_name} VARCHAR,
             CONSTRAINT {feature_name}_pk PRIMARY KEY (id_{feature_name})
             );""")
+    
+    # features 테이블 생성 (id_year와 범주형 테이블들의 자식 테이블)
     cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_list[-1]} (
         id VARCHAR NOT NULL,
         age INTEGER,
@@ -90,6 +112,10 @@ def table_creation(cursor, table_list):
         CONSTRAINT {table_list[-1]}_fk_{table_list[-2]} FOREIGN KEY ({table_list[-2]}) REFERENCES {table_list[-2]} (id_{table_list[-2]})
         );""")
 
+
+# csv data를 RDB에 입력하는 기능
+# csv에서 한줄씩 넣는 executemany 방식은 시간이 상당히 오래 걸림
+# cursor의 copy_from 메소드로 csv전체를 복사하여 RDB에 붙여 넣으면 훨씬 간단하고 빠르게 수행할 수 있음!
 def insertion_csv(cursor, table_name, path):
     '''
     insertion_csv
@@ -102,9 +128,13 @@ def insertion_csv(cursor, table_name, path):
     ---
     출력 : None 
     '''
+    # 연산 실시후 파일이 닫히도록 with문을 활용한다
     with open(path, 'r') as cf:
+        # csv 모듈의 reader를 통해 csv 파일을 읽기
         csv_reader = csv.reader(cf)
+        # 첫 행에는 변수 이름이 저장되어 있으므로 next로 넘어가기
         next(csv_reader)
+        # copy_from으로 csv데이터를 통째로 입력하기
         cursor.copy_from(cf, table_name, sep=',')
 
 def insertion_json(cursor, table_list, path):
@@ -119,8 +149,10 @@ def insertion_json(cursor, table_list, path):
     ---
     출력 : None 
     '''
+    # 연산 실시후 파일이 닫히도록 with문을 활용한다
     with open(path, 'r', encoding='utf-8') as jf:
         json_reader = json.load(jf)
+    # 전달받은 table list의 요소를 순서대로 반복하여 json 데이터를 입력
     for table_name in table_list:
         cursor.executemany(f"""INSERT INTO {table_name} (id_{table_name}, {table_name}) VALUES (%s, %s);""",
                            [list(dict.values()) for dict in json_reader[table_name]])
