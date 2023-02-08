61c9943da17764a0f66ea8f68537ae717bcace8f
KyungJae Cheong <dankj1991@gmail.com>
2023-02-08 20:51:05 +0900

    Deploy version 23.02.08

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # 라이브러리 및 모듈함수 불러오기
 from flask import Flask, render_template, request
+import json
 from modules_for_app import *
 
 # Flask app 지정
@@ -30,105 +31,139 @@ def prediction():
     # POST request(submit으로부터 실행)
     if request.method == 'POST':
         try:
-        # Request Form 으로부터 변수를 가져오는 과정부터 실시함
+            # Request Form 으로부터 변수를 가져오는 과정부터 실시함
+            names_request = list()  # 변수 이름을 담을 리스트
+            dict_request = dict()   # 변수 값을 담을 딕셔너리
             
-            # 범주형_1 (Gender ~ Subjective Health) (Q1 ~ Q6, 6개)
-            # Q 1. Gender [1,2]
-            gender = int(request.form['gender'])
-            # Q 2. Education [1,2,3,4]
-            education = int(request.form['edu'])            
-            # Q 3. Household [0,1,2,3]
-            household = int(request.form['household'])
-            # Q 4. Marital status [1,2,3]
-            marital = int(request.form['marital'])
-            # Q 5. Employment [1,2]
-            economy = int(request.form['economy'])
-            # Q 6. Subjective Health [1,2,3,4,5]
-            health = int(request.form['health'])
-            
-            # 이진형 (Limitation ~ Dyslipidemia) (Q7 ~ Q13, 7개)
-            # Q 7. Limitation [0,1]
-            limitation = int(request.form['limitation'])
-            # Q 8. Modality [0,1]
-            modality = int(request.form['modality'])
-            # Q 9. Weight Change [0,1]
-            w_change = int(request.form['w_change'])
-            # Q10. Weight Control [0,1]
-            w_control = int(request.form['w_control'])
-            # Q11. Hypertension [0,1]
-            high_bp = int(request.form['high_bp'])
-            # Q12. Diabetes [0,1]
-            diabetes = int(request.form['diabetes'])
-            # Q13. Dyslipidemia [0,1]
-            high_lipid = int(request.form['high_lipid'])
-            
-            # 범주형_2 (Drink Frequency ~ Stress) (Q14 ~ Q17, 4개)
-            # Q14. Drink Frequency [0,1,2,3,4,5,6]
-            drink_freq = int(request.form['drk_freq'])
-            # Q15. Drink Amount [0,1,2,3,4,5]
-            drink_amount = int(request.form['drk_amount'])
-            # Q16. Smoking [0,1]
-            smoking = int(request.form['smoke'])
-            # Q17. Stress Cognition [4,3,2,1]
-            stress = int(request.form['stress'])
-            
-            # 수치형 (Age, Height, Weight) (Q18 ~ 19, 3개)
+            # 수치형 변수 (Q18 ~ 19, 총 3개) : 변수별로 dtype이 다르기 때문에 따로 진행
+            names_numeric = ['age', 'height', 'weight']
+            dict_numeric = dict()   # 변수 값을 담을 딕셔너리
             # Q18. Age : integer (19 ~ 80)
-            age = int(request.form['age'])
+            dict_numeric[names_numeric[0]] = int(request.form[names_numeric[0]])
             # Q19. Height : float (100 ~ 200)
-            height = float(request.form['height'])
-            # Weight : float (30 ~ 150)
-            weight = float(request.form['weight'])
+            dict_numeric[names_numeric[1]] = float(request.form[names_numeric[1]])
+            # Q19. Weight : float (30 ~ 150)
+            dict_numeric[names_numeric[2]] = float(request.form[names_numeric[2]])
+            # 전체리스트 및 딕셔너리 업데이트
+            names_request.extend(names_numeric)
+            dict_request.update(dict_numeric)
             
-            # 수집한 변수들을 List로 묶어주기(수치형, 이진형, 범주형 순서)
-            list_numeric = [age, height, weight]
-            list_binary = [limitation, modality, w_change, w_control, high_bp, diabetes, high_lipid]
-            list_category = [gender, education, household, marital, economy, health,
-                             drink_freq, drink_amount, smoking, stress]
+            # 이진형 변수 (Q7 ~ Q13, 7개) : 모두 integer이기 for문으로 진행
+            names_binary = ['limitation', 'modality', 'w_change', 'w_control',
+                            'high_bp', 'diabetes', 'high_lipid']
+            dict_binary = dict()    # 변수 값을 담을 딕셔너리
+            # Q 7 ~ 13. 이진형 변수
+            for name in names_binary:
+                dict_binary[name] = int(request.form[name])
+            # 전체리스트 및 딕셔너리 업데이트
+            names_request.extend(names_binary)
+            dict_request.update(dict_binary)
             
+            # 범주형 변수 (Q1 ~ Q6 & Q14 ~ Q17, 총 10개)
+            names_category = ['gender','edu','household','marital','economy','health',
+                              'drk_freq','drk_amount','smoke','stress']
+            dict_category = dict()  # 변수 값을 담을 딕셔너리
+            # Q1 ~ Q6 & Q14 ~ Q17. 범주형 변수
+            for name in names_category:
+                dict_category[name] = int(request.form[name])
+            # 전체리스트 및 딕셔너리 업데이트
+            names_request.extend(names_category)
+            dict_request.update(dict_category)
+            
+            # (Modeling용 데이터 생성)
+            # 수집한 변수 값들을 List로 묶어주기(수치형, 이진형, 범주형 순서)
+            values_numeric = [dict_numeric[name] for name in names_numeric]
+            values_binary = [dict_binary[name] for name in names_binary]
+            values_category = [dict_category[name] for name in names_category]
             # Feature 조합 및 Encoding 실시
-            list_encoded_numeric = Encoding_for_model(list_numeric, mode='numeric')
-            list_encoded_binary = Encoding_for_model(list_binary, mode='binary')
-            list_encoded_category = Encoding_for_model(list_category, mode='category')
-            # 리스트 병합
-            list_encoded = list_encoded_numeric + list_encoded_binary + list_encoded_category
+            values_encoded_numeric = Encoding_for_model(values_numeric, mode='numeric')
+            values_encoded_binary = Encoding_for_model(values_binary, mode='binary')
+            values_encoded_category = Encoding_for_model(values_category, mode='category')
+            # 리스트 병합 (Global 변수로 지정하여 다른 함수에서 사용할 수 있도록 함)
+            global values_encoded
+            values_encoded = values_encoded_numeric + values_encoded_binary + values_encoded_category
             
-            # 최종 Model들 불러오기
-            dir_depr = './tuning-models/CNN_depr.h5'
-            dir_mdd = './tuning-models/MLP_mdd.h5'
-            # model_depr, model_mdd = model_loads(dir_depr, dir_mdd)        # 배포 과정에서 CPU메모리초과 발생
+            # (입력 Check용 데이터 생성)
+            # 전체 변수이름과 변수값을 묶어서 딕셔너리 정의하기
+            values_request = [dict_request[name] for name in names_request]
+            dict_values = dict(zip(names_request, values_request))
             
-            # 우울증 예측 (정상vs우울증)
-            # pred_depr, prob_depr = pred_prob(model_depr, list_encoded)    # 배포 과정에서 CPU메모리초과 발생
-            prob_depr, pred_depr = model_pred_prob(dir_depr, list_encoded)
-            # 확률값을 퍼센트로 변환 (둘째자리까지 표기)
-            percent_depr = int(round(prob_depr,4)*10000)/100
+            # 이진형과 범주형은 Label이 따로 존재하므로 이를 정리한 JSON파일로 리스트 생성
+            names_bin_cat = names_binary + names_category
+            # JSON file 불러오기
+            with open('./form_labels.json', 'r', encoding='utf-8') as jf:
+                json_reader = json.load(jf)
+            # 이진형과 범주형 Label 리스트 생성
+            labels_bin_cat = list()
+            for name in names_bin_cat:
+                label = json_reader[name][f"{dict_values[name]}"]
+                labels_bin_cat.append(label)
             
-            # 우울증이 있는 경우 : 주요우울장애(MDD) 예측 (경도우울증vs주요우울장애)
-            if pred_depr == 1:
-                # pred_mdd, prob_mdd = pred_prob(model_mdd, list_encoded)   # 배포 과정에서 CPU메모리초과 발생
-                prob_mdd, pred_mdd = model_pred_prob(dir_mdd, list_encoded)
-                # 확률값을 퍼센트로 변환 (둘째자리까지 표기)
-                percent_mdd = int(round(prob_mdd,4)*10000)/100
-            # 우울증이 없는 경우 : 주요우울장애(MDD) 예측 X
-            elif pred_depr == 0:
-                prob_mdd, pred_mdd, percent_mdd = None, None, None
+            # 수치형 변수값 리스트를 Label값을 담은 리스트와 병합하여 Check용 딕셔너리 생성
+            check_request = values_numeric + labels_bin_cat
+            dict_check = dict(zip(names_request, check_request))
             
-            # pred_mdd를 문자열로 이용하기위해 변환하는 과정(결과에 따라 다르게 출력하기 위함)
-            mdd_class = str()
-            if pred_mdd == 0:
-                mdd_class = "경도우울증"
-            elif pred_mdd == 1:
-                mdd_class = "주요우울장애"
-            
-            # 오류없이 예측을 마친 경우 : templates/result.html 실행(200)
-            return render_template('result.html',
-                                   pred_depr=pred_depr, prob_depr=percent_depr,
-                                   mdd_class=mdd_class, prob_mdd=percent_mdd), 200
+            # 오류없이 입력이 된 경우 : templates/prediction.html에 리스트 전달(200)
+            return render_template('prediction.html', checklist=dict_check), 200
         
-        # try문에서 에러가 발생한 경우 : templates/404.html 실행(404)
+        # try문에서 에러가 발생한 경우 : templates/404-2.html 실행(404)
         except:
             return render_template('404-2.html'), 404
+
+# Depression 예측 실행 함수(POST)
+@app.route('/result', methods=['POST'])
+def result():
+    # POST request(submit으로부터 실행)
+    if request.method == 'POST':
+        pred_mode = request.form['predict_type']
+        if pred_mode == 'depr':
+            try:
+                # model file directory 지정
+                depr_dir = './tuning-models/CNN_depr.h5'
+                
+                # global 변수에 저장한 인코딩된 변수리스트를 불러옴
+                list_encoded = values_encoded
+                
+                # 우울증 예측 (정상vs우울증) : 확률값은 재사용을 위해 global 변수에 저장
+                global percent_depr
+                prob_depr, pred_depr = model_pred_prob(depr_dir, data=list_encoded)
+                # 확률값을 소수점 둘째자리까지 퍼센트로 변환
+                percent_depr = int(round(prob_depr, 4)*(10**4))/100
+                
+                # 오류없이 학습이 진행된 경우 : templates/result.html 실행(200)
+                return render_template('result.html', pred_depr=pred_depr, prob_depr=percent_depr), 200
+            # try문에서 에러가 발생한 경우 : templates/404-2.html 실행(404)
+            except:
+                return render_template('404-2.html'), 404
+        if pred_mode == 'mdd':
+            try:
+                # model file directory 지정
+                mdd_dir = './tuning-models/MLP_mdd.h5'
+                
+                # global 변수에 저장한 변수들을 불러옴
+                list_encoded = values_encoded
+                proba_depr = percent_depr
+                
+                # 주요우울장애 예측 (경도우울증vs주요우울장애) : 재사용하지 않기 때문에 global저장은 안함
+                prob_mdd, pred_mdd = model_pred_prob(mdd_dir, data=list_encoded)
+                # 확률값을 소수점 둘째자리까지 퍼센트로 변환
+                percent_mdd = int(round(prob_mdd, 4)*(10**4))/100
+                
+                # 예측한 클래스를 문자열로 이용하기 위해 변환함
+                mdd_class = str()
+                if pred_mdd == 0:
+                    mdd_class = "경도우울증"
+                elif pred_mdd == 1:
+                    mdd_class = "주요우울장애"
+                
+                # 오류없이 학습이 진행된 경우 : templates/result.html 실행(200)
+                return render_template('result.html',
+                                       pred_depr=None, prob_depr=proba_depr,
+                                       mdd_class=mdd_class, prob_mdd=percent_mdd), 200
+                
+            # try문에서 에러가 발생한 경우 : templates/404-2.html 실행(404)
+            except:
+                return render_template('404-2.html'), 404
 
 # dashboard page 실행함수(GET)
 @app.route('/dashboard', methods=['GET'])
